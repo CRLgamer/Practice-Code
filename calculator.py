eb0c61d76ee8f57d9c9a0a0837c346ffa3207e11
CRLgamer <139924906+CRLgamer@users.noreply.github.com>
2025-10-02 20:47:08 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,45 +1,47 @@
 def add():
-  x = float(input("Enter the first number: ")
-  y = float(input("Enter the second number: ")
   total = x + y
   print(f"{x} + {y} = {total}")
   return total
 
 def subtract():
-  x = float(input("Enter the first number: ")
-  y = float(input("Enter the second number: ")
   total = x - y
-  print(f"{x} + {y} = {total}")
+  print(f"{x} - {y} = {total}")
   return total
 
 def multiply():
-  x = float(input("Enter the first number: ")
-  y = float(input("Enter the second number: ")
   total = x * y
-  print(f"{x} + {y} = {total}")
+  print(f"{x} * {y} = {total}")
   return total
 
 def divide():
-  x = float(input("Enter the first number: ")
-  y = float(input("Enter the second number: ")
   total = x / y
-  print(f"{x} + {y} = {total}")
+  print(f"{x} / {y} = {total}")
+  return total
+
+def mod():
+  total = x % y
+  print(f"{x} % {y} = {total}")
   return total
 
 print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
+print("5. Mod")
 
 oper = int(input("Choose the operation: "))
+x = float(input("Enter the first number: ")
+y = float(input("Enter the second number: ")
 
 if oper == 1:
-  add()
+  add(x, y)
 elif oper == 2:
-  subtract()
+  subtract(x, y)
 elif oper == 3:
-  multiply()
+  multiply(x, y)
 elif oper == 4:
-  divide()
+  divide(x, y)
+elif oper == 5:
+  mod(x, y)
 else:
   print("Invalid Number. Please try again.)
